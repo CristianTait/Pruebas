5d6462ab9c2edf8dd5d63eaea4bff6ffb5bd5be6
Cristian Tait <90878440+CristianTait@users.noreply.github.com>
2023-08-25 13:05:01 -0300

    Update pluginupdate.php

--- pluginupdate.php
+++ pluginupdate.php
@@ -1,17 +1,9 @@
 <form method="post">
-    <div class="form-group">
-        <label for="user_to_add" class="font-weight-bold">Agregar usuario habilitado:</label>
-        <select class="form-control" name="user_to_add" id="user_to_add" size="10">
-            <?php foreach ($all_user_vendor as $user) : ?>
-                <option value="<?php echo esc_attr($user->user_login); ?>"><?php echo esc_html($user->user_login); ?></option>
-            <?php endforeach ?>
-        </select>
-    </div>
-    <button type="submit" class="btn btn-primary btn-lg" name="add_user">Agregar</button>
-</form>
-
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+			<label for="user_to_remote">Eliminar usuario autorizado:</label>
+			<select name="user_to_remove" id="user_to_remove" >
+				<?php foreach ($authorized_users as $user) : ?>
+					<option value="<?php echo esc_attr($user); ?>"><?php echo esc_html($user); ?></option>
+				<?php endforeach?>
+			</select>
+			<input type="submit" name="remove_user" value="eliminar">
+		</form>
