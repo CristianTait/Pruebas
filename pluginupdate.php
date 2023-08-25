1c4106d65a15a4efc288d8fa1b40cac4390dbe08
Cristian Tait <90878440+CristianTait@users.noreply.github.com>
2023-08-25 13:05:31 -0300

    Update pluginupdate.php

--- pluginupdate.php
+++ pluginupdate.php
@@ -1,9 +1,11 @@
 <form method="post">
-			<label for="user_to_remote">Eliminar usuario autorizado:</label>
-			<select name="user_to_remove" id="user_to_remove" >
-				<?php foreach ($authorized_users as $user) : ?>
-					<option value="<?php echo esc_attr($user); ?>"><?php echo esc_html($user); ?></option>
-				<?php endforeach?>
-			</select>
-			<input type="submit" name="remove_user" value="eliminar">
-		</form>
+    <div class="form-group">
+        <label for="user_to_remove" class="font-weight-bold">Eliminar usuario autorizado:</label>
+        <select class="form-control" name="user_to_remove" id="user_to_remove">
+            <?php foreach ($authorized_users as $user) : ?>
+                <option value="<?php echo esc_attr($user); ?>"><?php echo esc_html($user); ?></option>
+            <?php endforeach ?>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-danger btn-lg" name="remove_user">Eliminar</button>
+</form>
