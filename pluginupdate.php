9f4a667a8e8a10973afcf9109616096d56266cd5
Cristian Tait <90878440+CristianTait@users.noreply.github.com>
2023-08-25 13:03:49 -0300

    Update pluginupdate.php

--- pluginupdate.php
+++ pluginupdate.php
@@ -1,9 +1,11 @@
 <form method="post">
-			<label for="user_to_add">Agregar usuario habilitado: </label>
-			<select name="user_to_add" id="user_to_add" size="10">
-				<?php foreach ($all_user_vendor as $user) : ?>
-					<option value="<?php echo esc_attr($user->user_login); ?>"><?php echo esc_html($user->user_login); ?></option>
-				<?php endforeach?>
-			</select>
-			<input type="submit" name="add_user" value="Agregar">
-		</form>
+    <div class="form-group">
+        <label for="user_to_add" class="font-weight-bold">Agregar usuario habilitado:</label>
+        <select class="form-control" name="user_to_add" id="user_to_add" size="10">
+            <?php foreach ($all_user_vendor as $user) : ?>
+                <option value="<?php echo esc_attr($user->user_login); ?>"><?php echo esc_html($user->user_login); ?></option>
+            <?php endforeach ?>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary btn-lg" name="add_user">Agregar</button>
+</form>
