4da23486ac8135953f8c79dd9bd1cc814693757e
Cristian Tait <90878440+CristianTait@users.noreply.github.com>
2023-08-25 13:02:09 -0300

    Update pluginupdate.php

--- pluginupdate.php
+++ pluginupdate.php
@@ -1,11 +1,9 @@
-if (
-    in_array('administrator', $user_log->roles) or 
-    isset($user_log->caps['administrator']) or 
-    $user_log->caps['administrator'] == 1 or 
-    (in_array($user_log->user_login, $authorized_users) or 
-    in_array($allow_role, $user_log->roles))
-) {
-    echo ('Tienes acceso a la Actualizacion de Precios');
-} else {
-    echo ('No tienes acceso a la Actualizacion de Precios');
-}
+<form method="post">
+			<label for="user_to_add">Agregar usuario habilitado: </label>
+			<select name="user_to_add" id="user_to_add" size="10">
+				<?php foreach ($all_user_vendor as $user) : ?>
+					<option value="<?php echo esc_attr($user->user_login); ?>"><?php echo esc_html($user->user_login); ?></option>
+				<?php endforeach?>
+			</select>
+			<input type="submit" name="add_user" value="Agregar">
+		</form>
